213077193e9cc0e99dec28c3510c02eafd9650c6
Ulysses4ever <Ulysses4ever@localhost>
2011-09-30 21:03:36 +0300

    Computing rational points on Hermitian curve added.

--- CurveArithmetic.hpp
+++ CurveArithmetic.hpp
@@ -8,7 +8,11 @@
 #ifndef CURVEARITHMETIC_HPP_
 #define CURVEARITHMETIC_HPP_
 
+#include <functional>
+#include <iterator>
 #include <vector>
+
+#include <iostream>
 
 #include "Point.hpp"
 
@@ -24,8 +28,56 @@ getBasis(int l) {
     std::vector< Pt > result;
     result.reserve(l);
     Pt p;
+   // std::function<Pt(int)> f(&Pt::operator++);
+    //std::generate_n(std::back_inserter(result), l,
+//            std::bind<Pt, Pt, int>(&Pt::operator++, p);//);
+//            std::bind(std::plus<int>(), 3);
     while (l-- > 0)
         result.push_back(p++);
+    return result;
+}
+
+template<typename FieldElem, typename CurvePoint>
+bool isPlainHermitianCurveRationalPoint(int r, CurvePoint cp) {
+    // Hermitian curve equation --v
+    return FieldElemTraits<FieldElem>::power(cp[0], r + 1) -
+            FieldElemTraits<FieldElem>::power(cp[1], r) - cp[1] ==
+                    FieldElemTraits<FieldElem>::addId();
+    // TODO: repetitive computations of x^n are ineffective
+}
+
+template<typename CurvePoint, typename FieldElem>
+std::vector<CurvePoint>
+getPlainHermitianCurveRationalPoints(int r, FieldElem x) {
+//    auto power = &FieldElemTraits<FieldElem>::power;
+    std::vector<CurvePoint> result;
+    FieldElem zero = FieldElemTraits<FieldElem>::addId();
+    CurvePoint cp;
+    cp[0] = x;
+    do {
+        cp[1] = x;
+        do {
+            if (isPlainHermitianCurveRationalPoint<FieldElem>(r, cp)) {
+                result.push_back(cp);
+            }
+            cp[1] *= x;
+        } while (cp[1] != x);
+
+        cp[0] *= x;
+    } while (cp[0] != x);
+
+    cp[0] = zero;
+    cp[1] = x;
+    do {
+        if (isPlainHermitianCurveRationalPoint<FieldElem>(r, cp)) {
+            result.push_back(cp);
+        }
+        cp[1] *= x;
+    } while (cp[1] != x);
+
+    cp[0] = zero;
+    cp[1] = zero;
+    result.push_back(cp);
     return result;
 }
 
