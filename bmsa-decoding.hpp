01a30a77b4bdfb09efb214d53029b544af7c0eb2
Ulysses4ever <Ulysses4ever@localhost>
2011-10-05 21:06:21 +0300

    ECCCodeTraits added as template parameter and OrderPolicy parameter removed ? most info about types came from ECCCodeTraits now.
    CurvePoint not need so far (come from CodeTraits).
    
    Syndrom calculation added ? not that correct so far.

--- bmsa-decoding.hpp
+++ bmsa-decoding.hpp
@@ -12,6 +12,8 @@
 #include <map>
 #include <vector>
 
+#include <boost/iterator/transform_iterator.hpp>
+
 #include "Point.hpp"
 #include "bmsa.hpp"
 #include "mv_poly.hpp"
@@ -19,31 +21,40 @@
 
 namespace mv_poly {
 
-template<typename T, int N>
-struct CurvePoint {
-    std::array<T, N> data;
-};
+//template<typename T, int N>
+//struct CurvePoint {
+//    std::array<T, N> data;
+//};
 
 template<
     int Dim,
     typename Field,
-    template <typename PointImpl> class OrderPolicy = GradedAntilexMonomialOrder
+    typename ECCodeTraits/*,
+    template <typename> class OrderPolicy = GradedAntilexMonomialOrder*/
 >
 class BMSDecoding {
 
 public:
 
-    typedef std::vector< CurvePoint<Field, Dim> > CurvePointsCollection;
+    typedef typename ECCodeTraits::BasisElem BasisElem;
+
+    typedef typename ECCodeTraits::CurvePoint CurvePoint;
+
+    typedef std::vector< CurvePoint > CurvePointsCollection;
 
     typedef std::vector<Field> FieldElemsCollection;
 
 private:
 
-    typedef std::map< Point<Dim, OrderPolicy>, Field> SyndromeType;
+//    typedef Point<Dim, OrderPolicy> LatticePoint;
 
-    size_t a;
+    typedef std::map< BasisElem, Field> SyndromeType;
 
-    CurvePointsCollection points;
+    size_t l;
+
+    CurvePointsCollection curvePoints;
+
+
 
     //FieldElemsCollection * r = 0;
 
@@ -67,36 +78,59 @@ private:
 
     }
 
-    void computeErrorLocatorPolynomials(FieldElemsCollection const & r) {
-        int i = 0;
-        Point<Dim, OrderPolicy> k, synLength;
+    void computeErrorLocatorPolynomials(FieldElemsCollection const & received) {
         SyndromeType syn;
-        // compute known syndrome length on the basis of code parameter 'a'
-        for (int i = 0; i < a; ++i) {
-            syn[k] = in_prod(expPoints(k), r);
-            ++k;
-        }
+        auto basis = ECCodeTraits::getCodeBasis(l);
+
+        using namespace std::tr1::placeholders;
+        auto syndromComponentAtBasisElem =
+            [this,&received](BasisElem const & be) -> typename SyndromeType::value_type {
+                auto tit = boost::make_transform_iterator(this->curvePoints.begin(),
+                        std::tr1::bind(
+                            computeMonomAtPoint<Field, BasisElem, CurvePoint>,
+                            be,
+                            _1));
+                return typename SyndromeType::value_type(
+                        be,
+                        std::inner_product(received.begin(), received.end(),
+                            tit, FieldElemTraits<Field>::addId()));
+
+        };
+        std::transform(basis.begin(), basis.end(),
+                std::inserter(syn, syn.begin()), syndromComponentAtBasisElem);
+        std::for_each(syn.begin(), syn.end(),
+                [](typename SyndromeType::value_type const & s) {
+            std::cout << makeNtlPowerPrinter(s.second) << " ";
+
+        });
+        //        auto sn = f(basis[0]);
+        //        std::cout << "syn[0]: " << //makeNtlPowerPrinter(
+        //                   sn/*)*/ << std::endl;
+
         //BMSAlgorithm< SyndromeType, MVPolyType<Dim, Field>::type >
             // bmsa(syn, synLength);
 
-        while(true) { // TODO: define stop condition
-            frmv();
-            ++k; // TODO: ++ should be correctly implemented for given OrderPolicy
-        }
+//        while(true) { // TODO: define stop condition
+//            frmv();
+//            ++k; // TODO: ++ should be correctly implemented for given OrderPolicy
+//        }
     }
 
 public:
 
     BMSDecoding(
-            size_t a,
-            CurvePointsCollection const & points)
-    : a(a), points(points) {}
+            size_t l)
+    : l(l) {
+        curvePoints = ECCodeTraits::getRationalPoints();
+    }
 
     FieldElemsCollection decode(FieldElemsCollection const & r) {
+        FieldElemsCollection result;
         computeErrorLocatorPolynomials(r);
-        CurvePointsCollection locations = getErrorLocations();
-        FieldElemsCollection values = getErrorValues(locations);
+//        CurvePointsCollection locations = getErrorLocations();
+//        FieldElemsCollection values = getErrorValues(locations);
         // correct r with locators and values...
+        return result;
     }
 }; // BMSDecoding
 
